57f264bd5a45d45ca9b96e9061a36d55b1d8995c
Yasir <design@yasirsaike.com>
2025-08-28 17:38:53 -0700

    Create missing index.html and main.js files

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Neon Glass Menu</h1>
+    <p class="read-the-docs">
+      Your project is ready to go!
+    </p>
+  </div>
+`
